270087a404ae3eac7f8e3697bcf365bb8a903d04
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-05-15 20:33:52 +0530

    Update Q5.py

--- Input/Q5.py
+++ Input/Q5.py
@@ -2,7 +2,9 @@ print ("Can a Triangle be Formed?")
 Side1 = int(input("Enter First Side :"))
 Side2 = int(input("Enter Second Side :"))
 Side3 = int(input("Enter Third side :"))
-if Side1 <(Side2 + Side3) and Side2 <(Side3 + Side1) and Side3 <(Side1 + Side2) :
+while Side1 <(Side2 + Side3) and Side2 <(Side3 + Side1) and Side3 <(Side1 + Side2) :
     print ("Yes")
-else :
-    print ("No")
+    break
+else:
+    print("No")
+    
