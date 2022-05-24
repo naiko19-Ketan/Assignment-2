70c94700185392fe0f286c518ea94d4e24c1bc1f
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-05-24 13:31:09 +0530

    Update Q6.py

--- Input/Q6.py
+++ Input/Q6.py
@@ -1,8 +1,9 @@
 def countSetBits( n ):
     count = 0
-    while n:
-        count += 1
-        n &= (n-1)
+    while n!=0:
+        if n%2==1 :
+            count += 1
+        n //= 2
     return count
      
 
